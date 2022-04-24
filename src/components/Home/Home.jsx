f14529a216e7cf2bbf425eef87991c76ed1bc224
qayjjj <trngquanh@gmail.com>
2022-04-24 00:32:43 -0500

    add players manually

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,9 +8,27 @@ import "./styles.css";
 function Home() {
   const { setStartGame, setPlayers } = DareState.useContainer();
   const [inputValid, setInputValid] = useState(false);
+  const [names, setNames] = useState([""]);
 
-  const handleOnChange = (e) => {
-    if (e.target.value) {
+  const handleAddPlayer = () => {
+    setNames(names.concat([""]));
+  };
+
+  const handleRemovePlayer = (index) => {
+    const temp = [...names];
+    temp.splice(index, 1);
+    setNames(temp);
+  };
+
+  const checkInput = (input) => {
+    return input == "" || input.trim().length === 0;
+  };
+
+  const handleInputChange = (e, index) => {
+    const temp = [...names];
+    temp[index] = e.target.value;
+    const validity = temp.every(checkInput);
+    if (!validity) {
       if (!inputValid) {
         setInputValid(true);
         document
@@ -25,64 +43,47 @@ function Home() {
           .classList.remove("start-button-clickable");
       }
     }
+    setNames(temp);
   };
 
-  const handleOnClick = () => {
-    const input = document.getElementById("name-input").value;
-    const names = input.replaceAll(/\s/g, "").split(",");
+  const handleStartGame = () => {
     let players = [];
-    names.map((name) => players.push({ name: name, score: 0 }));
+    names.map((name) => {
+      if (name !== "") {
+        players.push({ name: name.trim(), score: 0 });
+      }
+    });
     setStartGame(true);
     setPlayers(players);
   };
 
-  const handleAddPlayer = () => {
-    let newPlayerInput = document.querySelector(".name-input").cloneNode(true);
-    newPlayerInput.firstChild.value = "";
-    newPlayerInput.lastChild.onclick = handleRemovePlayer;
-    document.querySelector("#name-input-fields").append(newPlayerInput);
-  };
-
-  const handleRemovePlayer = (e) => {
-    let currentInput = e.target.parentNode;
-    currentInput.remove();
-  };
-
   return (
     <div className="home-container flex flex-col items-center mt-32">
-      <div className="relative">
-        <h1 className="header text-5xl drop-shadow-lg font-semibold">
-          Who are the players?
-        </h1>
-        {/* <img
-          src={arrow}
-          className="h-20 absolute -right-44 top-2.5"
-          alt="arrow.png"
-        /> */}
+      <h1 className="header text-5xl drop-shadow-lg font-semibold">
+        Who are the players?
+      </h1>
+
+      <div className="mt-6 w-1/4 text-black text-xl flex flex-col items-center relative">
+        {names.map((name, index) => (
+          <div className="name-input relative w-full flex">
+            <input
+              value={name}
+              placeholder="Enter player's name"
+              className="input-field mt-2 w-full h-10 rounded-full drop-shadow-lg py-5 px-8 relative"
+              onChange={(e) => handleInputChange(e, index)}
+            ></input>
+            <span
+              className="remove-button text-4xl rotate-45 absolute -right-8 top-1 cursor-pointer drop-shadow-lg"
+              onClick={() => handleRemovePlayer(index)}
+            >
+              +
+            </span>
+          </div>
+        ))}
       </div>
 
       <div
-        id="name-input-fields"
-        className="mt-6 w-1/4 text-black text-xl flex flex-col items-center relative"
-      >
-        <div  className="name-input relative w-full flex">
-          <input
-          id="name-input"
-            placeholder="Enter player's name"
-            className="input-field mt-2 w-full h-10 rounded-full drop-shadow-lg py-5 px-8 relative"
-            onChange={(e) => handleOnChange(e)}
-          ></input>
-          <span
-            className="remove-button text-4xl rotate-45 absolute -right-8 top-1 cursor-pointer drop-shadow-lg"
-            onClick={(e) => handleRemovePlayer(e)}
-          >
-            +
-          </span>
-        </div>
-      </div>
-
-      <div
-        className="add-player-button 'w-1/4 sm:w-1/6 lg:w-1/2' mt-4 rounded-full drop-shadow-lg h-10 cursor-pointer grid place-items-center"
+        className="add-player-button w-1/4 mt-4 rounded-full drop-shadow-lg h-10 cursor-pointer grid place-items-center"
         onClick={() => handleAddPlayer()}
       >
         <span className="text-5xl font-bold plus-sign -mt-2">+</span>
@@ -90,8 +91,8 @@ function Home() {
 
       <div
         id="start-button"
-        className="start-button 'w-1/5 sm:w-1/8 lg:w-1/3' mt-12 h-20 px-12 rounded-full flex place-content-center items-center justify-between"
-        onClick={inputValid ? handleOnClick : null}
+        className="start-button 'w-1/4 sm:w-1/8 lg:w-1/3' mt-12 h-20 px-12 rounded-full flex place-content-center items-center justify-between"
+        onClick={inputValid ? handleStartGame : null}
       >
         <span className="start-text text-5xl font-extrabold tracking-widest">
           Start
